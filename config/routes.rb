d49721d4d57a5e09a46587381f8dc204e824938d
Grain Stark <palagend@gmail.com>
2018-03-22 16:27:48 +0800

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
+  get 'static_pages/contact'
+
   get 'static_pages/help'
 
   get 'static_pages/home'
 
   get 'static_pages/about'
 
-  get 'welcome/index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
+  root 'static_pages#home'
 end
