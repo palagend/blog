cfb1e76ba5b56d6cac0dae1c35e6350f086acab8
Grain Stark <palagend@gmail.com>
2018-05-16 18:59:01 +0800

    Updateing user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,17 @@
 # coding: utf-8
 class UsersController < ApplicationController
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  def edit
+    @user = User.find params[:id]
+  end
   def new
     @user = User.new
   end
